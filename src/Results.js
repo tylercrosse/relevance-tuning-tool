71f99edc9231173320dc64622f8141f1ed1dbc1d
Tyler Crosse <tyler.crosse@knowable.com>
2022-12-19 12:45:03 -0800

    Use ACE to render results

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import useLocalStorage from "./useLocalStorage";
 import Table from "./Table";
 import ColumnSelector from "./ColumnSelector";
+import AceEditor from "react-ace";
 
 /**
  * Responsible for rendering the results of a query
@@ -30,11 +31,27 @@ export default function Results(props) {
       />
       <Table columns={columns} data={props.hits || []} />
       <h3>Raw Results</h3>
-      <pre className="results_raw">
-        <code>
-          {JSON.stringify({ ...props.raw, hits: props.hits }, null, 2)}
-        </code>
-      </pre>
+      <AceEditor
+        placeholder="Placeholder Text"
+        width="100%"
+        maxLines={Infinity}
+        mode="json"
+        theme="textmate"
+        name="results"
+        readOnly
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={JSON.stringify({ ...props.raw, hits: props.hits }, null, 2)}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: false,
+          enableSnippets: false,
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+      />
     </section>
   );
 }
